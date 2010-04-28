24a90ebc7dfa3bde0fd30a7b61826b1a8275ceb3
Tomas Restrepo <tomas@winterdom.com>
2010-04-27 21:52:00 -0500

    Separating language-specific tests

---
+++ Languages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Winterdom.VisualStudio.Extensions.Text {
+  interface IMarkupLanguage {
+    bool IsDelimiter(String tagName);
+    bool IsName(String tagName);
+    bool IsAttribute(String tagName);
+  }
+  class XmlMarkup : IMarkupLanguage {
+    public bool IsDelimiter(String tagName) {
+      return tagName == "XML Delimiter";
+    }
+    public bool IsName(String tagName) {
+      return tagName == "XML Name";
+    }
+    public bool IsAttribute(String tagName) {
+      return tagName == "XML Attribute";
+    }
+  }
+  class XamlMarkup : IMarkupLanguage {
+    public bool IsDelimiter(String tagName) {
+      return tagName == "XAML Delimiter";
+    }
+    public bool IsName(String tagName) {
+      return tagName == "XAML Name";
+    }
+    public bool IsAttribute(String tagName) {
+      return tagName == "XAML Attribute";
+    }
+  }
+  class HtmlMarkup : IMarkupLanguage {
+    public bool IsDelimiter(String tagName) {
+      return tagName == "HTML Tag Delimiter" || tagName == "HTML Operator";
+    }
+    public bool IsName(String tagName) {
+      return tagName == "HTML Element Name";
+    }
+    public bool IsAttribute(String tagName) {
+      return tagName == "HTML Attribute Name";
+    }
+  }
+}
