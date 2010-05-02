8d45bf31f949f9416a92224c11e1dc7a06ec901c
Tomas Restrepo <tomas@winterdom.com>
2010-05-02 13:09:43 -0500

    Almost resolves namespaces correctly now!

--- QuickInfo.cs
+++ QuickInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Xml;
 using System.Text;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
@@ -10,6 +11,7 @@ using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
+using System.IO;
 
 namespace Winterdom.VisualStudio.Extensions.Text {
   [Export(typeof(IQuickInfoSourceProvider))]
@@ -56,7 +58,7 @@ namespace Winterdom.VisualStudio.Extensions.Text {
 
       if ( CheckForPrefixTag(tagAggregator, extent.Span) ) {
         string text = extent.Span.GetText();
-        string url = "unknown";
+        string url = FindNSUri(extent.Span, GetDocText(extent.Span));
         applicableToSpan = currentSnapshot.CreateTrackingSpan(
           extent.Span, SpanTrackingMode.EdgeInclusive
         );
@@ -64,6 +66,31 @@ namespace Winterdom.VisualStudio.Extensions.Text {
       }
     }
 
+    private String FindNSUri(SnapshotSpan span, String docText) {
+      String subtext = docText;
+      int endElem = docText.IndexOf('>', span.Span.End);
+      if ( endElem > 0 && endElem < docText.Length - 1) {
+        subtext = docText.Substring(0, endElem+1); 
+      }
+      StringReader sr = new StringReader(subtext);
+      XmlReaderSettings settings = new XmlReaderSettings();
+      settings.ConformanceLevel = ConformanceLevel.Fragment;
+      XmlReader reader = XmlReader.Create(sr, settings);
+      String thisPrefix = span.GetText();
+      String lastUriForPrefix = null;
+      try {
+        while ( reader.Read() ) {
+          if ( reader.Prefix == thisPrefix ) {
+            lastUriForPrefix = reader.NamespaceURI;
+          }
+        }
+      } catch ( Exception ex ) {
+      }
+      return String.IsNullOrEmpty(lastUriForPrefix) ? "unknown" : lastUriForPrefix;
+    }
+    private String GetDocText(SnapshotSpan span) {
+      return span.Snapshot.GetText();
+    }
     private bool CheckForPrefixTag(
         ITagAggregator<ClassificationTag> tagAggregator,
         SnapshotSpan span) {
@@ -87,6 +114,22 @@ namespace Winterdom.VisualStudio.Extensions.Text {
     }
 
     public void Dispose() {
+    }
+  }
+
+  internal class CountingStringReader : StringReader {
+    public int Position { get; private set; }
+    public CountingStringReader(String text) : base(text) {
+    }
+    public override int Read() {
+      int val = base.Read();
+      if ( val >= 0 ) Position++;
+      return val;
+    }
+    public override int Read(char[] buffer, int index, int count) {
+      int read = base.Read(buffer, index, count);
+      Position += read;
+      return read;
     }
   }
 }
