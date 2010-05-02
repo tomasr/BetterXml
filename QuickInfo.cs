f0a7bcc7b85b38796850a8a0d574db705101b930
Tomas Restrepo <tomas@winterdom.com>
2010-05-02 12:31:17 -0500

    Refactoring + Tooltip only shows on prefixes

--- QuickInfo.cs
+++ QuickInfo.cs
@@ -21,101 +21,64 @@ namespace Winterdom.VisualStudio.Extensions.Text {
     [Import]
     internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
     [Import]
-    internal ITextBufferFactoryService TextBufferFactoryService { get; set; }
-    
+    internal IViewTagAggregatorFactoryService AggregatorFactory { get; set; }
+
     public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer) {
-      return new XmlQuickInfoSource(textBuffer, NavigatorService);
-    }
-  }
-
-  [Export(typeof(IIntellisenseControllerProvider))]
-  [Name("BetterXml QuickInfo Controller")]
-  [ContentType(Constants.CT_XML)]
-  [ContentType(Constants.CT_XAML)]
-  internal class XmlQuickInfoControllerProvider : IIntellisenseControllerProvider {
-    [Import]
-    internal IQuickInfoBroker QuickInfoBroker { get; set; }
-  
-    public IIntellisenseController TryCreateIntellisenseController(
-        ITextView textView, IList<ITextBuffer> subjectBuffers) {
-      return new XmlQuickInfoController(textView, subjectBuffers, QuickInfoBroker);
-    }
-  }
-
-  internal class XmlQuickInfoController : IIntellisenseController {
-    private ITextView textView;
-    private IList<ITextBuffer> textBuffers;
-    private IQuickInfoSession session;
-    private IQuickInfoBroker quickInfoBroker;
-
-    internal XmlQuickInfoController(
-        ITextView textView, IList<ITextBuffer> textBuffers,
-        IQuickInfoBroker quickInfoBroker) {
-      this.textView = textView;
-      this.textBuffers = textBuffers;
-      this.quickInfoBroker = quickInfoBroker;
-
-      textView.MouseHover += OnTextViewMouseHover;
-    }
-    public void Detach(ITextView textView) {
-      if ( this.textView == textView ) {
-        textView.MouseHover -= this.OnTextViewMouseHover;
-        textView = null;
-      }
-    }
-    public void ConnectSubjectBuffer(ITextBuffer subjectBuffer) {
-      textBuffers.Add(subjectBuffer);
-    }
-
-    public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer) {
-      textBuffers.Remove(subjectBuffer);
-    }
-    void OnTextViewMouseHover(object sender, MouseHoverEventArgs e) {
-      SnapshotPoint? point = textView.BufferGraph.MapDownToFirstMatch(
-        new SnapshotPoint(textView.TextSnapshot, e.Position),
-        PointTrackingMode.Positive,
-        snapshot => textBuffers.Contains(snapshot.TextBuffer),
-        PositionAffinity.Predecessor
-      );
-      if ( point != null ) {
-        ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(
-          point.Value.Position, PointTrackingMode.Positive);
-        if ( quickInfoBroker.IsQuickInfoActive(textView) ) {
-          session = quickInfoBroker.TriggerQuickInfo(textView, triggerPoint, true);
-        }
-      }
+      return new XmlQuickInfoSource(textBuffer, this);
     }
   }
 
   internal class XmlQuickInfoSource : IQuickInfoSource {
     private ITextBuffer textBuffer;
-    private ITextStructureNavigatorSelectorService navSelector;
+    private XmlQuickInfoSourceProvider provider;
 
-    public XmlQuickInfoSource(ITextBuffer buffer, ITextStructureNavigatorSelectorService nav) {
+    public XmlQuickInfoSource(ITextBuffer buffer, XmlQuickInfoSourceProvider provider) {
       this.textBuffer = buffer;
-      this.navSelector = nav;
+      this.provider = provider;
     }
     public void AugmentQuickInfoSession(
-        IQuickInfoSession session, IList<object> quickInfoContent, 
+        IQuickInfoSession session, IList<object> quickInfoContent,
         out ITrackingSpan applicableToSpan) {
 
-      SnapshotPoint? subjectTriggerPoint = 
+      applicableToSpan = null;
+      SnapshotPoint? subjectTriggerPoint =
         session.GetTriggerPoint(textBuffer.CurrentSnapshot);
       if ( !subjectTriggerPoint.HasValue ) {
-        applicableToSpan = null;
         return;
       }
       ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
       SnapshotSpan querySpan = new SnapshotSpan(subjectTriggerPoint.Value, 0);
 
-      ITextStructureNavigator navigator = navSelector.GetTextStructureNavigator(textBuffer);
+      var tagAggregator = GetAggregator(session);
+      ITextStructureNavigator navigator =
+        provider.NavigatorService.GetTextStructureNavigator(textBuffer);
       TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
       string searchText = extent.Span.GetText();
 
-      applicableToSpan = currentSnapshot.CreateTrackingSpan(
-        extent.Span, SpanTrackingMode.EdgeInclusive
+      if ( CheckForPrefixTag(tagAggregator, extent.Span) ) {
+        applicableToSpan = currentSnapshot.CreateTrackingSpan(
+          extent.Span, SpanTrackingMode.EdgeInclusive
+        );
+        quickInfoContent.Add("This is a tooltip");
+      }
+    }
+
+    private bool CheckForPrefixTag(
+        ITagAggregator<ClassificationTag> tagAggregator,
+        SnapshotSpan span) {
+      foreach ( var tagSpan in tagAggregator.GetTags(span) ) {
+        String tagName = tagSpan.Tag.ClassificationType.Classification;
+        if ( tagName == Constants.XML_PREFIX ) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private ITagAggregator<ClassificationTag> GetAggregator(IQuickInfoSession session) {
+      return provider.AggregatorFactory.CreateTagAggregator<ClassificationTag>(
+        session.TextView
       );
-      quickInfoContent.Add("This is a tooltip");
     }
 
     public void Dispose() {
